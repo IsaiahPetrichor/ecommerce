8b9750601b367f6ed23b0f6e10cd5602368fe9d2
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2022-02-04 17:14:49 -0800

    fixed database schema to work with Postgres

--- server/E-Commerce App.sql
+++ server/E-Commerce App.sql
@@ -1,101 +1,80 @@
-CREATE TABLE "user" (
-  "id" integer PRIMARY KEY,
-  "first_name" varchar(24),
-  "last_name" varchar(24),
-  "email" varchar(36),
-  "password" varchar(40),
-  "phone" integer,
-  "created_on" timestamp
+CREATE TABLE users(
+	id integer PRIMARY KEY,
+  first_name varchar(32) NOT NULL,
+  last_name varchar(32) NOT NULL,
+  email varchar(60) NOT NULL,
+  password varchar(40) NOT NULL,
+  phone integer,
+  created_on timestamp,
+  UNIQUE(email)
 );
 
-CREATE TABLE "user_address" (
-  "user_id" integer PRIMARY KEY,
-  "line_1" varchar,
-  "line_2" varchar,
-  "city" varchar,
-  "postal" varchar,
-  "country" varchar
+CREATE TABLE user_address (
+  user_id integer PRIMARY KEY REFERENCES users(id),
+  line_1 varchar NOT NULL,
+  line_2 varchar,
+  city varchar NOT NULL,
+  postal varchar NOT NULL,
+  country varchar NOT NULL
 );
 
-CREATE TABLE "user_payment" (
-  "id" integer PRIMARY KEY,
-  "user_id" integer,
-  "type" varchar,
-  "provider" varchar,
-  "card_number" integer,
-  "expires" date
+CREATE TABLE user_payment (
+  id integer PRIMARY KEY,
+  user_id integer REFERENCES users(id) NOT NULL,
+  type varchar NOT NULL,
+  provider varchar NOT NULL,
+  card_number integer NOT NULL,
+  expires date NOT NULL
 );
 
-CREATE TABLE "order_details" (
-  "id" integer PRIMARY KEY,
-  "user_id" integer,
-  "payment_id" integer,
-  "payment_status" varchar,
-  "total" decimal,
-  "date" date
+CREATE TABLE order_details (
+  id integer PRIMARY KEY,
+  user_id integer REFERENCES users(id) NOT NULL,
+  payment_id integer REFERENCES user_payment(id) NOT NULL,
+  payment_status varchar NOT NULL,
+  total decimal NOT NULL,
+  date date NOT NULL
 );
 
-CREATE TABLE "order_items" (
-  "id" integer PRIMARY KEY,
-  "order_id" integer,
-  "product_id" integer,
-  "quantity" integer
+CREATE TABLE order_items (
+  id integer PRIMARY KEY,
+  order_id integer REFERENCES order_details(id) NOT NULL,
+  product_id integer REFERENCES product(id) NOT NULL,
+  quantity integer NOT NULL
 );
 
-CREATE TABLE "cart_session" (
-  "id" integer PRIMARY KEY,
-  "user_id" integer,
-  "total" decimal
+CREATE TABLE cart_session (
+  id integer PRIMARY KEY,
+  user_id integer REFERENCES users(id) NOT NULL,
+  total decimal NOT NULL
 );
 
-CREATE TABLE "cart_item" (
-  "id" integer PRIMARY KEY,
-  "cart_session_id" integer,
-  "product_id" integer,
-  "quantity" integer
+CREATE TABLE cart_item (
+  id integer PRIMARY KEY,
+  cart_session_id integer REFERENCES cart_session(id) NOT NULL,
+  product_id integer REFERENCES product(id) NOT NULL,
+  quantity integer NOT NULL
 );
 
-CREATE TABLE "product" (
-  "id" integer PRIMARY KEY,
-  "name" varchar,
-  "description" text,
-  "SKU" varchar(40),
-  "category_id" integer,
-  "inventory_id" integer,
-  "price" decimal,
-  "created_on" timestamp
+CREATE TABLE product (
+  id integer PRIMARY KEY,
+  name varchar NOT NULL,
+  description text NOT NULL,
+  SKU varchar(40) NOT NULL,
+  category_id integer REFERENCES product_category(id) NOT NULL,
+  inventory_id integer REFERENCES product_inventory(id) NOT NULL,
+  price decimal NOT NULL,
+  created_on timestamp NOT NULL
 );
 
-CREATE TABLE "product_category" (
-  "id" integer PRIMARY KEY,
-  "name" varchar,
-  "desc" text
+CREATE TABLE product_category (
+  id integer PRIMARY KEY,
+  name varchar NOT NULL,
+  description text NOT NULL
 );
 
-CREATE TABLE "product_inventory" (
-  "id" integer PRIMARY KEY,
-  "quantity" integer,
-  "updated" timestamp
+CREATE TABLE product_inventory (
+  id integer PRIMARY KEY,
+  quantity integer NOT NULL,
+  updated timestamp NOT NULL
 );
-
-ALTER TABLE "user_address" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "user" ADD FOREIGN KEY ("id") REFERENCES "user_payment" ("user_id");
-
-ALTER TABLE "order_details" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "order_details" ADD FOREIGN KEY ("payment_id") REFERENCES "user_payment" ("id");
-
-ALTER TABLE "order_items" ADD FOREIGN KEY ("order_id") REFERENCES "order_details" ("id");
-
-ALTER TABLE "order_items" ADD FOREIGN KEY ("product_id") REFERENCES "product" ("id");
-
-ALTER TABLE "cart_session" ADD FOREIGN KEY ("user_id") REFERENCES "user" ("id");
-
-ALTER TABLE "cart_item" ADD FOREIGN KEY ("cart_session_id") REFERENCES "cart_session" ("id");
-
-ALTER TABLE "product" ADD FOREIGN KEY ("id") REFERENCES "cart_item" ("product_id");
-
-ALTER TABLE "product" ADD FOREIGN KEY ("category_id") REFERENCES "product_category" ("id");
-
-ALTER TABLE "product" ADD FOREIGN KEY ("inventory_id") REFERENCES "product_inventory" ("id");
