da23e3c2bbcc10d830f599ca41706a33e7b84772
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2022-02-08 19:19:20 -0800

    added custom middleware and cleaned some code to follow DRY principle

--- server/api/user.js
+++ server/api/user.js
@@ -6,72 +6,48 @@ const user = Router();
 
 // get all users
 user.get('/', async (req, res, next) => {
-	try {
-		const users = await pool.query('SELECT * FROM users');
-		res.send(users.rows);
-	} catch (e) {
-		console.log(e.stack);
-	}
+	const users = await pool.query('SELECT * FROM users');
+	res.send(users.rows);
 });
 
 // get a single user by uuid
 user.get('/:id', async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		const user = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
-		if (user.rows[0]) {
-			res.send(user.rows[0]);
-		} else {
-			res.status(404).send();
-		}
-	} catch (e) {
-		console.log(e.stack);
-		res.status(400).send('Bad request...');
+	const { id } = req.params;
+	const user = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+	if (user.rows[0]) {
+		res.send(user.rows[0]);
+	} else {
+		res.status(404).send();
 	}
 });
 
 user.post('/', async (req, res, next) => {
-	try {
-		const { first_name, last_name, email, password, phone } = req.body;
-		const newUser = await pool.query(
-			'INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
-			[first_name, last_name, email, password, req.date, phone, uuidv4()]
-		);
-		res.status(201).json(newUser.rows[0]);
-	} catch (e) {
-		console.log(e.stack);
-		res.status(400).send('Bad request...');
-	}
+	const { first_name, last_name, email, password, phone } = req.body;
+	const newUser = await pool.query(
+		'INSERT INTO users VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+		[first_name, last_name, email, password, req.date, phone, uuidv4()]
+	);
+	res.status(201).json(newUser.rows[0]);
 });
 
 user.put('/:id', async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		const body = req.body;
-		// for loop to only send updates for the filled parameters
-		for (let key in body) {
-			if (body[key] !== '') {
-				// uses pg-format in order to allow dynamic queries without risk of SQL injection
-				const sql = format('UPDATE users SET %I = $1 WHERE id = $2', key);
-				await pool.query(sql, [body[key], id]);
-			}
+	const { id } = req.params;
+	const body = req.body;
+	// for loop to only send updates for the filled parameters
+	for (let key in body) {
+		if (body[key] !== '') {
+			// uses pg-format in order to allow dynamic queries without risk of SQL injection
+			const sql = format('UPDATE users SET %I = $1 WHERE id = $2', key);
+			await pool.query(sql, [body[key], id]);
 		}
-		res.status(201).send('User updated');
-	} catch (e) {
-		console.log(e.stack);
-		res.status(400).send('Bad request...');
 	}
+	res.status(201).send('User updated');
 });
 
 user.delete('/:id', async (req, res, next) => {
-	try {
-		const { id } = req.params;
-		pool.query('DELETE FROM users WHERE id = $1', [id]);
-		res.sendStatus(204);
-	} catch (e) {
-		console.log(e.stack);
-		res.status(404).send('Delete failed');
-	}
+	const { id } = req.params;
+	pool.query('DELETE FROM users WHERE id = $1', [id]);
+	res.sendStatus(204);
 });
 
 export default user;
---
+++ server/api/user_payment.js
@@ -0,0 +1,65 @@
+import pool from '../database/pool.js';
+import { v4 as uuidv4 } from 'uuid';
+import format from 'pg-format';
+import { authMiddleware } from '../custom_middleware.js';
+import { Router } from 'express';
+const userPayment = Router();
+
+userPayment.use(authMiddleware);
+
+// get all payments for a user
+userPayment.get('/', async (req, res, next) => {
+	const payments = await pool.query(
+		'SELECT * FROM user_payment WHERE user_id = $1',
+		[user_id]
+	);
+	res.send(payments.rows);
+});
+
+// get a single payment for a user
+userPayment.get('/:id', async (req, res, next) => {
+	const { payment_id } = req.params;
+	const payment = await pool.query(
+		'SELECT * FROM user_payment WHERE id = $1 AND user_id = $2',
+		[payment_id, user_id]
+	);
+	if (payment.rows[0]) {
+		res.send(payment.rows[0]);
+	} else {
+		res.status(404).send();
+	}
+});
+
+// add a new payment
+userPayment.post('/', async (req, res, next) => {
+	const { type, provider, card_number, expiration, user_id } = req.body;
+	const newPayment = await pool.query(
+		'INSERT INTO user_payment VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+		[type, provider, card_number, expiration, user_id, uuidv4()]
+	);
+	res.status(201).json(newPayment.rows[0]);
+});
+
+// update a payment
+userPayment.put('/:id', async (req, res, next) => {
+	const { payment_id } = req.params;
+	const body = req.body;
+	// for loop to only send updates for the filled parameters
+	for (let key in body) {
+		if (body[key] !== '') {
+			// uses pg-format in order to allow dynamic queries without risk of SQL injection
+			const sql = format('UPDATE user_payment SET %I = $1 WHERE id = $2', key);
+			await pool.query(sql, [body[key], payment_id]);
+		}
+	}
+	res.status(201).send('User updated');
+});
+
+// delete a payment
+userPayment.delete('/:id', async (req, res, next) => {
+	const { payment_id } = req.params;
+	pool.query('DELETE FROM user_payment WHERE id = $1', [payment_id]);
+	res.sendStatus(204);
+});
+
+export default userPayment;
