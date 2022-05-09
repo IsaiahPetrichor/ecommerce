f6134ca6b9b8826252ed9cb85436236dab43d913
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2022-05-08 17:39:54 -0700

    basic filtering added

--- client/src/products/products.tsx
+++ client/src/products/products.tsx
@@ -1,12 +1,12 @@
 import { FC, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import Filters from './filters';
 import './products.css';
 
 interface Product {
 	id: string;
 	name: string;
 	price: number;
+	category_id: string;
 }
 
 interface Category {
@@ -19,11 +19,18 @@ const Products: FC = () => {
 	const [products, setProducts] = useState<Product[]>([]);
 	const [categories, setCategories] = useState<Category[]>([]);
 
+	const [lowHigh, setLowHigh] = useState(true);
+	const [highLow, setHighLow] = useState(false);
+
 	useEffect(() => {
 		fetch('/api/products')
 			.then((res) => res.json())
 			.then((json) => {
-				setProducts(json.products);
+				setProducts(
+					json.products.sort((a: Product, b: Product) => {
+						return a.price - b.price;
+					})
+				);
 				setCategories(json.categories);
 			})
 			.catch((err) => {
@@ -31,9 +38,50 @@ const Products: FC = () => {
 			});
 	}, []);
 
+	const handleLowHigh = () => {
+		setLowHigh(!lowHigh);
+		if (highLow) setHighLow(false);
+
+		const productArr = [...products];
+		setProducts(
+			productArr.sort((a, b) => {
+				return a.price - b.price;
+			})
+		);
+	};
+
+	const handleHighLow = () => {
+		setHighLow(!highLow);
+		if (lowHigh) setLowHigh(false);
+
+		const productArr = [...products];
+		setProducts(
+			productArr.sort((a, b) => {
+				return b.price - a.price;
+			})
+		);
+	};
+
 	return (
 		<main className="products">
-			<Filters props={categories} />
+			<div className="filters">
+				<hr className="right-hr" />
+				<div id="filter-menu">
+					<div className="filter-header">
+						<h2 className="filter-title">Filters</h2>
+						<button>Clear</button>
+					</div>
+					<hr />
+					<label className="checkbox">
+						Sort by price (low to high):
+						<input type="checkbox" checked={lowHigh} onChange={handleLowHigh} />
+					</label>
+					<label className="checkbox">
+						Sort by price (high to low):
+						<input type="checkbox" checked={highLow} onChange={handleHighLow} />
+					</label>
+				</div>
+			</div>
 			<div className="products-list">
 				{products.map((product) => {
 					return (
