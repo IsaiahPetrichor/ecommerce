5e1aac90e6d50ed81d84bab651ef130c6ac9fd58
Isaiah Leach <30422715+FallenVFX@users.noreply.github.com>
2022-05-10 21:28:07 -0700

    ssl object for production

--- database/pool.js
+++ database/pool.js
@@ -12,6 +12,9 @@ const devConfig = {
 
 const productionConfig = {
 	connectionString: process.env.DATABASE_URL,
+	ssl: {
+		rejectUnauthorized: false,
+	},
 };
 const pool = new Pool(
 	process.env.NODE_ENV === 'production' ? productionConfig : devConfig
